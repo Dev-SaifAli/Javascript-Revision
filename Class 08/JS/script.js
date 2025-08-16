22961f8a63b0de0f17a3f935a3df6eddd6871e3b
SAIF ALI RABBANI <saifu100@outlook.com>
2025-08-16 08:41:19 +0500

    guess number

--- Class 08/JS/script.js
+++ Class 08/JS/script.js
@@ -52,8 +52,35 @@ function doWhile() {
 
   do {
     password = +prompt("Enter your password to login");
-    output.innerHTML = password;
+    // output.innerHTML = password;
   } while (password !== 12345);
 
-  alert("Access granted");
+  output.innerText = "Access granted!";
+}
+
+var i = 6;
+do {
+  console.log(i);
+  i++;
+} while (i < 5);
+console.log("False condition");
+
+// Guess number
+function guessNumber() {
+  let attempts = 0;
+  let randomNumber = Math.floor(Math.random() * 10 + 1);
+  console.log("randomNumber :>> ", randomNumber); // 2
+  let userGuessNumber; // declare variable
+  do {
+    attempts++;
+    userGuessNumber = +prompt("Guess the number");
+    console.log("attempts :>> ", attempts);
+    console.log("userGuessNumber :>> ", userGuessNumber); // 3
+    if (userGuessNumber !== randomNumber && attempts < 3) {
+      alert("Wrong guess, try again");
+    }
+
+    // alert("Wrong guess, try again");
+  } while (userGuessNumber !== randomNumber && attempts < 3);
+  alert("Congratulations! You guessed it right!");
 }
