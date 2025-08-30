4b266a076bdf3b3f78448d8b3c1906cedbf05075
SAIF ALI RABBANI <saifu100@outlook.com>
2025-08-30 17:59:53 +0500

    form

--- Class 10/JS/script.js
+++ Class 10/JS/script.js
@@ -10,6 +10,10 @@ let user = {
     longitude: 2654.454,
     latitude: 1545.154,
   },
+  fullName: function () {
+    // function inside the user is called object.
+    return this.firstName + " " + this.lastName;
+  },
 };
 console.log("user :>> ", user);
 
@@ -67,10 +71,75 @@ marks.forEach((a) => {
 });
 
 const names = ["Ali", "Ahmad", "Amina Shafique", "Munir", "Naseer"];
-names.forEach((value, array) => {
+names.forEach((value, index) => {
   console.log(`${index}:${value} `);
 });
 
 // names to hogia array name
 // forEach method array
 // parameters that fetch values from the array such as 1st param is actual value , 2nd is the index, and the 3rd is complete array
+
+console.log("user.fullName :>> ", user.fullName());
+
+// Show Output
+function showOutput(output) {
+  document.getElementById("outputTable").innerHTML = output;
+}
+
+// Clear Output
+function clearOutput() {
+  document.getElementById("outputTable").innerHTML = "";
+}
+
+// Notification
+
+function showNotification(message) {
+  Toastify({
+    text: message,
+    duration: 3000,
+    destination: "https://github.com/apvarun/toastify-js",
+    newWindow: true,
+    close: true,
+    gravity: "bottom", // `top` or `bottom`
+    position: "left", // `left`, `center` or `right`
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    style: {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    },
+    onClick: function () {}, // Callback after click
+  }).showToast();
+}
+
+// Get Field Value
+
+let getFieldValue = (ID) => {
+  return document.getElementById(ID).value;
+};
+const emailFormat = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+function handleSubmit() {
+  event.preventDefault();
+  let firstName = getFieldValue("firstName");
+  let lastName = getFieldValue("lastName");
+  let email = getFieldValue("email");
+  let dob = getFieldValue("dob");
+  console.log(firstName);
+
+  firstName = firstName.trim();
+  lastName = lastName.trim();
+  email = email.trim();
+  console.log(firstName);
+
+  if (firstName.length < 3) {
+    showNotification("Please enter your first name correctly", "error");
+  }
+  if (lastName.length < 3) {
+    showNotification("Please enter your last name correctly", "error");
+  }
+  if (!emailFormat.test(email)) {
+    showNotification("Please enter your email correctly", "error");
+  }
+  if (!dob) {
+    showNotification("Please enter your date of birth", "error");
+  }
+}
--- Class 10/points.md
+++ Class 10/points.md
@@ -1,8 +1,8 @@
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-function show image modal from bstrap show output table clear output
+ 
 globally
-alert 
+ 
 
 handle submit function > event preventDefault()
 trim method used 
